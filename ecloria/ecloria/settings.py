8482598f0473a95e5fc7c9276645afd657620c29
github.com/nabil-ghazali <nabil.dev.ghaz@hotmail.com>
2025-09-07 22:32:35 +0200

    Création account,  models Shopper et superUser (admin)

--- ecloria/ecloria/settings.py
+++ ecloria/ecloria/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog'
+    'catalog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+# Variable user (Shopper)
+AUTH_USER_MODEL = "accounts.Shopper"
