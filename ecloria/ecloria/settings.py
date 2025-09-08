3c4fe074419fd3ce7e370d521819a78c6e342d17
github.com/nabil-ghazali <nabil.dev.ghaz@hotmail.com>
2025-09-08 11:12:05 +0200

    Ajout apps dans ecloria/settings

--- ecloria/ecloria/settings.py
+++ ecloria/ecloria/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',     #gestion des utilisateurspo
+    'django.contrib.auth',     #gestion des utilisateurs
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
     'catalog',
-    'accounts',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -128,11 +129,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # Variable user (Shopper)
 AUTH_USER_MODEL = "accounts.Shopper"
+
+
+# CSS, JS, images
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Fichiers uploadés (produits)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
