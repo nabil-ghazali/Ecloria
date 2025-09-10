df60e7971bdb19514978d6b52db74a5e756d6bfe
Saleh-chanselme <saleh.chanselme@gmail.com>
2025-09-10 08:57:02 +0200

    search bottun

--- ecloria/static/js/script.js
+++ ecloria/static/js/script.js
@@ -7,3 +7,15 @@ const menu = document.querySelector('.tabs ul');
 toggle.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
+
+// search field 
+const searchBtn = document.getElementById("searchBtn");
+const searchInput = document.querySelector(".search-input");
+
+searchBtn.addEventListener("click", () => {
+    searchInput.classList.toggle("show");
+    if (searchInput.classList.contains("show")) {
+        searchInput.focus();
+    }
+});
+
